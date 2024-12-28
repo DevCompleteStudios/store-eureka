eeff74ad812a98c3630b9a1e2ca6b1f430bd468e
DevCompleteStudios <jorge.yael333@gmail.com>
2024-12-28 09:23:49 -0600

    Registrando y configurando servidor de eureka

--- src/main/java/com/devstudios/store/app/eureka/StoreEurekaApplication.java
+++ src/main/java/com/devstudios/store/app/eureka/StoreEurekaApplication.java
@@ -1,9 +1,13 @@
-package com.devstudios.store.app.eureka.store_eureka;
+package com.devstudios.store.app.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+
 
 @SpringBootApplication
+@EnableEurekaServer
 public class StoreEurekaApplication {
 
 	public static void main(String[] args) {
