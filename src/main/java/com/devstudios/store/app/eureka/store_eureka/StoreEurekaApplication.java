546d7408a7a2ff19adb0fb5664c45d2a3ea02a38
DevCompleteStudios <jorge.yael333@gmail.com>
2024-12-28 08:49:51 -0600

    First commit

---
+++ src/main/java/com/devstudios/store/app/eureka/store_eureka/StoreEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.devstudios.store.app.eureka.store_eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreEurekaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devstudios/store/app/eureka/store_eureka/StoreEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devstudios.store.app.eureka.store_eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
